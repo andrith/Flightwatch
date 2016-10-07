47b715325d33a25cc811965cfe3c94ae4dd77702
Vilhjalmur Gunnarsson <villi@tmsoftware.is>
2016-10-07 17:03:35 +0000

    Wrap everything in express and modify scraper

--- backend.js
+++ backend.js
@@ -9,89 +9,72 @@ var express = require('express');
 var app = express();
 var sio = require('socket.io');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "flightwatch",
-  password: "flightwatch",
-  database: "Flightwatch"
+const scraper = require('./scraper.js');
+
+app.get('/', function(req, res) {
+
+    parser.on('error', function(err) {
+        console.log('Parser error', err);
+    });
+
+    scraper.scraper()
+
+
+    // var j = schedule.scheduleJob('*/1 * * * *', function() {
+    //
+    //     var data = '';
+    //     https.get('https://test.icelandair.is/origo-portlets/rm/services/rm.xml?RequestType=departures&Departure=KEF&GapBefore=10&GapAfter=20&locale=', function(res) {
+    //         console.log(res)
+    //         if (res.statusCode >= 200 && res.statusCode < 400) {
+    //             res.on('data', function(data_) {
+    //                 data += data_.toString();
+    //             });
+    //             res.on('end', function() {
+    //                 parser.parseString(data, function(err, result) {
+    //                     for (i = 0; i < result.destinations.destination[0].flights[0].flight.length; i++) {
+    //                         var row = result.destinations.destination[0].flights[0].flight[i];
+    //                         console.log(row)
+    //                     }
+    //                 });
+    //             });
+    //         }
+    //     });
+    //
+    //
+    //     var data2 = '';
+    //     https.get('https://test.icelandair.is/origo-portlets/rm/services/rm.xml?RequestType=arrivals&Departure=KEF&GapBefore=10&GapAfter=20&locale=', function(res) {
+    //         console.log(res)
+    //         if (res.statusCode >= 200 && res.statusCode < 400) {
+    //             res.on('data', function(data_) {
+    //                 data2 += data_.toString();
+    //             });
+    //             res.on('end', function() {
+    //                 parser.parseString(data2, function(err, result) {
+    //                     for (i = 0; i < result.destinations.destination[0].flights[0].flight.length; i++) {
+    //                         var row = result.destinations.destination[0].flights[0].flight[i];
+    //                         console.log(row)
+    //                     }
+    //                 });
+    //             });
+    //         }
+    //     });
+    //
+    // });
+
+    // var io = sio.listen(app.listen(1234));
+    //   io.sockets.on('connection', function(socket) {
+    //     socket.on("getFlight", (data) => {
+    //       console.log(data);
+    //       con.query('SELECT * FROM flights WHERE flightNumber = ?' , data.flightNumber, function(err,dbRows){
+    //         socket.emit("flight", dbRows);
+    //       });
+    //     });
+    //   });
+
+    res.send();
+
 });
 
-parser.on('error', function(err) { console.log('Parser error', err); });
-
-
-var j = schedule.scheduleJob('*/15 * * * *', function(){
-
-    var data = '';
-    https.get('https://test.icelandair.is/origo-portlets/rm/services/rm.xml?RequestType=departures&Departure=KEF&GapBefore=10&GapAfter=20&locale=', function(res) {
-        if (res.statusCode >= 200 && res.statusCode < 400) {
-          res.on('data', function(data_) { data += data_.toString(); });
-          res.on('end', function() {
-            parser.parseString(data, function(err, result) {
-              for(i = 0; i < result.destinations.destination[0].flights[0].flight.length; i++){
-                var row = result.destinations.destination[0].flights[0].flight[i];
-                insertOrUpdateRow(row);
-              }
-            });
-          });
-        }
-      });
-
-
-    var data2 = '';
-    https.get('https://test.icelandair.is/origo-portlets/rm/services/rm.xml?RequestType=arrivals&Departure=KEF&GapBefore=10&GapAfter=20&locale=', function(res) {
-        if (res.statusCode >= 200 && res.statusCode < 400) {
-          res.on('data', function(data_) { data2 += data_.toString(); });
-          res.on('end', function() {
-            parser.parseString(data2, function(err, result) {
-              for(i = 0; i < result.destinations.destination[0].flights[0].flight.length; i++){
-                var row = result.destinations.destination[0].flights[0].flight[i];
-                insertOrUpdateRow(row);
-              }
-            });
-          });
-        }
-      });
-
-});
-
-  function insertOrUpdateRow(row){
-    con.query('SELECT * FROM flights WHERE flightNumber = ?' , row.flight_number[0], function(err,dbRows){
-      if(err) throw err;
-      if(dbRows.length > 0){
-        updateRow(row, dbRows);
-      } else {
-        insertRow(row);
-      }
-    });
-  }
-
-  function insertRow(row){
-    con.query('INSERT INTO flights VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', [null, row.flight_number[0], row.departure[0].scheduled[0], row.departure[0].estimate[0], row.departure[0].actual[0], row.departure[0].airport, row.departure[0].highlight[0], row.arrival[0].scheduled[0], row.arrival[0].estimate[0], row.arrival[0].actual[0], row.arrival[0].airport, row.arrival[0].highlight[0], row.status[0]]);
-  }
-
-  function updateRow(row, dbRows){
-    checkIfNewHighlight(row, dbRows);
-    con.query('DELETE FROM flights WHERE flightnumber = ?', row.flight_number[0])
-    con.query('INSERT INTO flights VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', [null, row.flight_number[0], row.departure[0].scheduled[0], row.departure[0].estimate[0], row.departure[0].actual[0], row.departure[0].airport, row.departure[0].highlight[0], row.arrival[0].scheduled[0], row.arrival[0].estimate[0], row.arrival[0].actual[0], row.arrival[0].airport, row.arrival[0].highlight[0], row.status[0]]);
-  }
-
-  function checkIfNewHighlight(row, dbRows){
-    console.log("CHECKING");
-    if(row.departure[0].highlight[0] == "true" && dbRows[0].dest_highlight == "true") {
-      console.log("NEW HIGHLIGHT");
-    }
-    if(row.arrival[0].highlight[0] == "true" && dbRows[0].arr_highlight == "true") {
-      console.log("NEW HIGHLIGHT");
-    }
-  }
-
-
-var io = sio.listen(app.listen(1234));
-io.sockets.on('connection', function(socket) {
-  socket.on("getFlight", (data) => {
-    console.log(data);
-    con.query('SELECT * FROM flights WHERE flightNumber = ?' , data.flightNumber, function(err,dbRows){
-      socket.emit("flight", dbRows);
-    });
-  });
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
--- scraper.js
+++ scraper.js
@@ -1,47 +1,45 @@
-var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 
-var app     = express();
+exports.scraper = () => {
+    url = 'http://www.kefairport.is/English/Timetables/Departures/';
 
-app.get('/status', function(req, res) {
+    request(url, function(error, response, html) {
 
-  url = 'http://www.kefairport.is/English/Timetables/Departures/';
+        if (!error) {
 
-  request(url, function(error, response, html) {
+            var $ = cheerio.load(html);
+            var date, flightNr, airline, destination, estimate, status;
+            var json = {
+                date: "",
+                flightNr: "",
+                airline: "",
+                destination: "",
+                estimate: "",
+                status: ""
+            };
 
-    if(!error) {
+            $('tbody tr').filter(function() {
 
-      var $ = cheerio.load(html);
-      var date, flightNr, airline, destination, estimate, status;
-      var json = { date: "", flightNr: "", airline: "", destination: "", estimate: "", status: "" };
+                var data = $(this);
 
-      $('tbody tr').filter(function() {
+                date = data.find("td:nth-of-type(1)").text();
+                flightNr = data.find("td:nth-of-type(2)").text();
+                airline = data.find("td:nth-of-type(3)").text();
+                destination = data.find("td:nth-of-type(4)").text();
+                estimate = data.find("td:nth-of-type(5)").text();
+                status = data.find("td:nth-of-type(6)").text();
 
-        var data = $(this);
+                json.date = date;
+                json.flightNr = flightNr;
+                json.airline = airline;
+                json.destination = destination;
+                json.estimate = estimate;
+                json.status = status;
 
-        date         = data.find("td:nth-of-type(1)").text();
-        flightNr     = data.find("td:nth-of-type(2)").text();
-        airline      = data.find("td:nth-of-type(3)").text();
-        destination  = data.find("td:nth-of-type(4)").text();
-        estimate     = data.find("td:nth-of-type(5)").text();
-        status       = data.find("td:nth-of-type(6)").text();
+                console.log(json);
 
-        json.date        = date;
-        json.flightNr    = flightNr;
-        json.airline     = airline;
-        json.destination = destination;
-        json.estimate    = estimate;
-        json.status      = status;
-
-        console.log(json);
-
-      })
-    }
-  })
-res.end()
-});
-
-app.listen(3000, function() {
-  console.log('Scraper listening on port 3000');
-});
+            })
+        }
+    })
+}
